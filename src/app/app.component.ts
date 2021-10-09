bedc3f1fbb05f7e27a9af6cd2a4b8937b4fcdef8
reginald <reginald@ntersol.com>
2021-10-09 00:33:25 -0500

    bfs algo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'algo';
+export class AppComponent implements OnInit {
+  public airPortName: string = '';
+  public destinationLoc: string = '';
+  public foundConnections: Array<string> = [];
+  public airports: Array<string> =
+    'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+  public routes: Array<string>[] = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC'],
+    ['JFK', 'HEL'],
+    ['JFK', 'LOS'],
+    ['MEX', 'LAX'],
+    ['MEX', 'BKK'],
+    ['MEX', 'LIM'],
+    ['MEX', 'EZE'],
+    ['LIM', 'BKK'],
+  ];
+  public adjacencyList: any;
+  public logList: any | undefined | null;
+  ngOnInit(): void {
+    this.adjacencyList = new Map();
+    this.airports.forEach((airport: string) => {
+      this.addNode(airport);
+    });
+    this.routes.forEach((route: any) => {
+      if (route) {
+        this.addEdge(route[0], route[1]);
+      }
+    });
+  }
+  public addNode(airport: string): void {
+    this.adjacencyList.set(airport, []);
+    this.logList = this.adjacencyList;
+  }
+  public addEdge(origin: any, destination: any) {
+    this.adjacencyList.get(origin).push(destination);
+    this.adjacencyList.get(destination).push(origin);
+  }
+
+  public bfs(): void {
+    this.foundConnections = [];
+    const visted = new Set();
+    const queue = [this.airPortName];
+    while (queue.length > 0) {
+      const airport = queue.shift();
+      const destinations = this.adjacencyList.get(airport);
+      for (const destination of destinations) {
+        if (destination === this.destinationLoc) {
+        console.log('found connection') 
+        }
+        if (!visted.has(destination)) {
+          visted.add(destination);
+          queue.push(destination);
+          console.log(destination)
+        }
+      }
+    }
+  }
 }
